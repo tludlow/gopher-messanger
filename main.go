285150de1e0dbacd6d9e6f6d7a909ac3fc37e231
tludlow99 <twjludlow@gmail.com>
2019-07-15 01:42:42 +0100

    Simple tcp server/client setup found on web tutorial, will expand to have my own server and client system next

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("[GOPHER] Starting program.")
+	//Here we will need to check whether the program should start as a client/server etc. probably through a argument in the command line on startup, e.g
+	//"./gopger-messanger --server <IP> <PORT>"
+}
---
+++ server/server.go
@@ -0,0 +1,3 @@
+package server
+
+//At first the only protocol supported will be HTTP messanging, which will be expanded in the future to more protocols.
